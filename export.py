915aae72afdd40c287a9423cc4f027e27a4bd88b
Salvo Miosi <s.miosi@reply.it>
2025-12-09 12:36:23 +0100

    cleanup get_rows

--- export.py
+++ export.py
@@ -1,4 +1,11 @@
 import graphql_api
+import html2text
+
+def check_chip(chip):
+    return ', '.join(chip) if chip else None
+
+def check_description(description):
+    return html2text.html2text(description).strip() if description else None
 
 def get_rows(site):
     return [
@@ -6,9 +13,9 @@ def get_rows(site):
             'page_id': page['id'],
             'page_name': page['name'],
             'carousel_id': playlist['id'],
-            'chip': [chip['name'] for chip in playlist['chip']],
+            'chip': check_chip([chip['name'] for chip in playlist['chip']]),
             'carousel_title': playlist['title'],
-            'carousel_description': playlist['description']
+            'carousel_description': check_description(playlist['description'])
         }
         for page in graphql_api.get_response(site)
         for row in page['grid']['rows']
